0b4815b03ab06227d64ab77040f019bb806af15e
Personal <personal@Michaels-MacBook-Pro-2.local>
2022-01-20 20:57:35 -0500

    Cleaned up packaging script

--- package.sh
+++ package.sh
@@ -4,5 +4,5 @@ mkdir .release
 mkdir .release/TalentSequence2
 cp *.toc .release/TalentSequence2/
 cp *.lua .release/TalentSequence2/
-cd .release/ && zip -r TalentSequence2-$(sed '3!d' ../IcyVeinsTalentSequence.toc | awk '{print $3}')-tbc-classic.zip . -x ".*" -x "__MACOSX"
-rm -rf "Talent Sequence 2.0"
+cd .release/ && zip -r TalentSequence2-$(sed '3!d' ../TalentSequence2.toc | awk '{print $3}')-tbc-classic.zip . -x ".*" -x "__MACOSX"
+rm -rf TalentSequence2
